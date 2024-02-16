6edde8362871d5aa025e3c903629feac13b5a51d
musa <musaopozturk11@gmail.com>
2024-02-16 23:18:10 +0300

    My Portfolio WebSite

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily : {
+        Inter: ['Inter', 'sans-serif'],
+        Poppins: ['Poppins','sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
