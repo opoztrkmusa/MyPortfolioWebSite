7c2ade2ac534aaeba7401c9596d80ecaed10ff2e
Berat Musa Öpöztürk <136088144+berat566@users.noreply.github.com>
2024-03-12 20:23:24 +0300

    Welcome My Portfolio Website

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const div = document.getElementById("page-wrapper");
+
+document.addEventListener("DOMContentLoaded",() =>{
+    setTimeout(() => {
+        div.style.top = "10px";
+        div.style.opacity = 1; 
+    }, 100);
+});
